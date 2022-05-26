90905fb7f0f2384de8f91940472bfe50e544d82d
Akshayku005 <akshaysportive@gmail.com>
2022-05-26 11:24:07 +0530

    Introducing servicelayer to AddressBook App

---
+++ src/main/java/com/bridgelabz/addressbookapp/service/AddressBookService.java
@@ -0,0 +1,56 @@
+package com.bridgelabz.addressbookapp.service;
+
+
+
+import com.bridgelabz.addressbookapp.dto.AddressBookDTO;
+import com.bridgelabz.addressbookapp.model.AddressBookData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AddressBookService implements IAddressBookService {
+
+    private List<AddressBookData> addressBookDataList=new ArrayList<>();
+
+    @Override
+    public List<AddressBookData> getAddressBookData() {
+        return addressBookDataList;
+    }
+
+    @Override
+    public AddressBookData getAddressBookDataById(int id) {
+        return addressBookDataList.get(id-1);
+    }
+    @Override
+    public AddressBookData updateAddressBookData(AddressBookDTO addressBookDTO) {
+        return null;
+    }
+
+    @Override
+    public AddressBookData createAddressBookData(AddressBookDTO addressBookDTO) {
+        AddressBookData addressBookData = new AddressBookData(addressBookDataList.size()+1,addressBookDTO);
+        addressBookDataList.add(addressBookData);
+        return addressBookData;
+    }
+
+    @Override
+    public AddressBookData updateAddressBookData(int id,AddressBookDTO addressBookDTO) {
+        AddressBookData addressBookData=this.getAddressBookDataById(id);
+        addressBookData.setfName(addressBookDTO.getfName());
+        addressBookData.setlName(addressBookDTO.getlName());
+        addressBookData.setPhonenumber(addressBookDTO.getPhonenumber());
+        addressBookData.setEmail(addressBookDTO.getEmail());
+        addressBookData.setCity(addressBookDTO.getCity());
+        addressBookData.setState(addressBookDTO.getState());
+        addressBookData.setZipcode(addressBookDTO.getZipcode());
+        addressBookDataList.set(id-1,addressBookData);
+        return addressBookData;
+    }
+
+    @Override
+    public void deleteEmployeePayrollData(int id) {
+        addressBookDataList.remove(id);
+    }
+
+}
