c1112f330540c32cd3e8ee44ca8a23f87de3f265
Akshayku005 <akshaysportive@gmail.com>
2022-05-27 23:01:10 +0530

    Added: sortByCity and SortByState

--- src/main/java/com/bridgelabz/addressbookapp/exception/AddressBookGlobalException.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.addressbookapp.exception;
-
-public class AddressBookGlobalException extends Exception{
-    public AddressBookGlobalException(String message) {
-        super(message);
-    }
-}
